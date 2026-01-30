08549d0f168eaf009d45d258b2961ca8fab6fca6
minuraanuradha <minura.aaj01@gmail.com>
2026-01-31 03:56:24 +0530

    fix:update featers categories sections images

--- assets/js/featured-categories.js
+++ assets/js/featured-categories.js
@@ -5,7 +5,7 @@ const data = {
         eyebrow: "Premium Products",
         description:
             "Savor the rich aroma and authentic taste of our handcrafted tea blends — crafted to calm, energize, and delight every moment.",
-        image: "assets/images/samples/image02.png",
+        image: "assets/images/feat_categories/Organic Teas Cover.png",
         link: "products.html?category=green-tea"
     },
 
@@ -15,7 +15,7 @@ const data = {
         eyebrow: "Premium Products",
         description:
             "From fruit-infused coolers to revitalizing brews, our beverages are designed to refresh your body and spirit.",
-        image: "assets/images/instagram/post02.jpg",
+        image: "assets/images/feat_categories/Organic Beverages.png",
         link: "products.html?category=iced-coffee"
     },
 
@@ -25,7 +25,7 @@ const data = {
         eyebrow: "Premium Products",
         description:
             "Add natural depth and bold flavor to your meals with our sustainably sourced organic spices.",
-        image: "assets/images/samples/image04.png",
+        image: "assets/images/feat_categories/Organic Spices.png",
         link: "products.html?category=ceylon-organic-spices"
     },
 
@@ -35,7 +35,7 @@ const data = {
         eyebrow: "Premium Products",
         description:
             "Pure, tropical coconut creations crafted to nourish, refresh, and elevate everyday living.",
-        image: "assets/images/samples/image03.png",
+        image: "assets/images/feat_categories/Organic Coconut Based.png",
         link: "products.html?category=coconut-water"
     }
 };
